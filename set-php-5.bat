6f38412b2c1056f41b59322dc1668a8ff4f44ebe
gr8ref <56557670+gr8ref@users.noreply.github.com>
2019-11-21 22:59:59 +0100

    Add files via upload

---
+++ set-php-5.bat
@@ -0,0 +1,29 @@
+@echo off
+set root=c:\xampp\
+echo Stoping Apache
+%root%apache\bin\httpd.exe -k stop
+pause
+rmdir C:\xampp\phpMyAdmin
+mklink /J C:\xampp\phpMyAdmin C:\xampp\phpMyAdmin5
+rmdir C:\xampp\apache
+mklink /J C:\xampp\apache C:\xampp\apache5
+rmdir C:\xampp\php
+mklink /J C:\xampp\php C:\xampp\php5
+echo The version of PHP5 is set
+echo Starting Apache ...
+
+%root%apache\bin\httpd.exe -k start
+
+if errorlevel 255 goto finish
+if errorlevel 1 goto error
+goto finish
+
+:error
+echo.
+echo Apache konnte nicht gestartet werden
+echo Apache could not be started
+pause
+:finish
+echo Starting mysql ...
+%root%mysql\bin\mysqld.exe --defaults-file=%root%mysql\bin\my.ini --standalone --console
+pause
